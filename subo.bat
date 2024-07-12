b755960541b1a0607012dd95f755377d0d8fe227
mmarq <mmarquez210@gmail.com>
2024-07-12 18:23:24 -0300

    ultimo commit

--- subo.bat
+++ subo.bat
@@ -5,4 +5,4 @@ set GOOS=linux
 set GOARCH=amd64
 set CGO_ENABLED=0
 go build -tags lambda.norpc -o bootstrap main.go
-%USERPROFILE%\Go\bin\build-lambda-zip.exe -o myFunction.zip bootstrap
+%USERPROFILE%\Go\bin\build-lambda-zip.exe -o main.zip bootstrap

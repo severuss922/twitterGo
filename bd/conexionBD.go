006df8d734827bc380c5e09b57a8af8a9577e730
mmarq <mmarquez210@gmail.com>
2024-07-12 18:40:42 -0300

    ultimo commit

--- bd/conexionBD.go
+++ bd/conexionBD.go
@@ -16,7 +16,7 @@ func ConectarBD(ctx context.Context) error {
 	user := ctx.Value(models.Key("user")).(string)
 	passwd := ctx.Value(models.Key("password")).(string)
 	host := ctx.Value(models.Key("host")).(string)
-
+	fmt.Println("> Intentando conectarse a BD")
 	connStr := fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=mayority", user, passwd, host)
 
 	var clientOptions = options.Client().ApplyURI(connStr)
